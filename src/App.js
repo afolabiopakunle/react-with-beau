37005b83ef8923650d10e710f160f4031e961886
afolabiopakunle <opakunleafolabi@gmail.com>
2022-09-03 09:15:42 +0100

    migrating to repl

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 import Header from './Header';
-import Content from './Content';
+import Employees from './Employees';
 import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css'
 export default function App() {
   return (
     <div className="container">
       <Header />
-      <Content />
+      <Employees />
       <Footer />
     </div>
   );
